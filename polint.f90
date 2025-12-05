e477c2f3ca13edf3ebe83edad6035ad9ea5e4ad5
Miguel Zilhao <mzilhao@ua.pt>
2025-12-05 16:56:46 +0000

    polint: better formatting and change comments to english

--- polint.f90
+++ polint.f90
@@ -1,73 +1,73 @@
 
-!  rotina 'polint', copiada do Numerical Recipes em C++, segunda edicao.  esta
-!  rotina efectua uma interpolacao ou extrapolacao polinomial.  ela aceita um
-!  vector 'xa', um vector 'ya' de pontos conhecidos e o ponto 'x' que se
-!  pretende interpolar (ou extrapolar), e devolve um double 'y', o valor da
-!  interpolacao polinomial no ponto 'x'.
+! Polynomial interpolation routine
+! Copied from Numerical Recipes in C++, 2nd edition.
+! Performs Lagrange polynomial interpolation/extrapolation.
+!
+! Input:
+!   xa, ya : known data points (arrays of size N)
+!   x      : point to interpolate at
+! Output:
+!   polint : interpolated value at x
 
-function polint( x, xa, ya )
+function polint(x, xa, ya)
   implicit none
 
-  double precision polint
+  double precision :: polint
   double precision, dimension(:), intent(in) :: xa, ya
   double precision, intent(in) :: x
 
-  integer i, m, ns, N
-  double precision ho, hp, den, diff, difftemp, w
+  integer :: i, m, ns, N
+  double precision :: ho, hp, den, diff, difftemp, w
   double precision, allocatable, dimension(:) :: C, D
 
   ns = 1
-  N  = size(ya)
+  N = size(ya)
 
-  allocate( C(N), D(N) )
+  allocate(C(N), D(N))
 
+  ! Find the closest point to x
   diff = abs(x - xa(1))
-  do i = 1, N                   ! achamos o ponto dado mais proximo de 'x'
+  do i = 1, N
+    difftemp = abs(x - xa(i))
+    if (difftemp < diff) then
+      ns = i
+      diff = difftemp
+    end if
 
-     difftemp = abs(x - xa(i))
-     if( difftemp < diff ) then
-        ns = i                  ! e guardamos a posicao em 'ns'.
-        diff = difftemp 
-     end if
-
-     C(i) = ya(i)               ! inicializamos os vectores C's e D's
-     D(i) = ya(i)
-
+    C(i) = ya(i)  ! Initialize C and D arrays
+    D(i) = ya(i)
   end do
- 
-  polint = ya(ns)               ! primeira aproximacao para polint: dizemos que e'
-                                ! igual ao valor do ponto mais proximo de 'x',
-                                ! obtido acima
+
+  polint = ya(ns)  ! Initial estimate
   ns = ns - 1
 
-  do m = 1, N - 1               ! 'm' percorre as colunas do quadro (ver NR)
-     do i = 1, N - m            ! para cada coluna calculamos todos os C's
-        ho = xa(i) - x          ! e D's.
-        hp = xa(i+m) - x
-        w = C(i+1) - D(i)
+  ! Build the interpolation table
+  do m = 1, N - 1
+    do i = 1, N - m
+      ho = xa(i) - x
+      hp = xa(i + m) - x
+      w = C(i + 1) - D(i)
 
-        den = ho - hp
-        ! este erro so' acontecera' se os dois xa's forem identicos (dentro 
-        ! da aproximacao numerica)
-        if( abs(den) < 1.d-12 ) then
-           write(*,*) "erro na rotina polint!"
-           stop
-        end if
+      den = ho - hp
+      ! Error if two xa values are identical (within numerical precision)
+      if (abs(den) < epsilon(den)*max(abs(ho), abs(hp))) then
+        write(*,*) "Error in polint: denominator too small (coincident xa values)"
+        stop
+      end if
 
-        den = w/den
-        D(i) = hp * den          ! actualizamos os C's e D's. 
-        C(i) = ho * den
-     end do
+      den = w/den
+      D(i) = hp*den   ! Update C and D
+      C(i) = ho*den
+    end do
 
-     if( 2 * ns < N - m ) then
-        polint = polint + C(ns+1)
-     else
-        polint  = polint + D(ns)
-        ns = ns - 1
-     end if
-
+    if (2*ns < N - m) then
+      polint = polint + C(ns + 1)
+    else
+      polint = polint + D(ns)
+      ns = ns - 1
+    end if
   end do
 
-  return
+  deallocate(C, D)
 
 end function polint
