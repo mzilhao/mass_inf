b9db6eba4c2be818beed602be20a092791f463fe
Miguel Zilhão <mzilhao@gmail.com>
2025-12-07 19:31:56 +0000

    evolve.F90: rename variables
    no functional changes

--- evolve.f90
+++ evolve.f90
@@ -3,50 +3,50 @@
 ! Solves: \partial_{uv} h_j(u,v) = F_j(h_k, \partial_u h_k, \partial_v h_k)
 !
 ! Input:
-!   hp1, hp2, hp3 : values at p1=(u,v), p2=(u,v+Dv), p3=(u+Du,v)
+!   h_S, h_E, h_W : values at S=(u,v), E=(u,v+Dv), W=(u+Du,v)
 !   Du, Dv        : step sizes in u and v directions
-!   F              : RHS subroutine with signature F(dhduv, h, dhdu, dhdv)
+!   F             : RHS subroutine with signature F(dhduv, h, dhdu, dhdv)
 ! Output:
-!   hp4            : solution at p4=(u+Du, v+Dv)
+!   h_N           : solution at N=(u+Du, v+Dv)
 !
-subroutine evolve(hp4, hp1, hp2, hp3, Du, Dv)
+subroutine evolve(h_N, h_S, h_E, h_W, Du, Dv)
   use functions
   implicit none
 
-  double precision, dimension(NN), intent(out) :: hp4
-  double precision, dimension(NN), intent(in)  :: hp1, hp2, hp3
+  double precision, dimension(NN), intent(out) :: h_N
+  double precision, dimension(NN), intent(in)  :: h_S, h_E, h_W
   double precision, intent(in)                 :: Du, Dv
 
   integer :: j
-  double precision, dimension(NN) :: hp5, dhdup5, dhdvp5, dhduvp5, dhduvp5new, hp4old
+  double precision, dimension(NN) :: h_P, dhdu_P, dhdv_P, dhduv_P, dhduv_P_new, h_N_old
 
-  ! First-order approximation for hp4, then compute derivatives at p5 = (u+Du/2, v+Dv/2)
-  hp4 = hp3 + hp2 - hp1
-  hp5 = 0.5d0*(hp1 + hp4)
-  !hp5 = 0.5*(hp2 + hp3)
-  !hp5 = 0.25*(hp1 + hp4 + hp2 + hp3)
-  dhdup5 = (hp3 - hp1 + hp4 - hp2)*0.5d0/Du
-  dhdvp5 = (hp2 - hp1 + hp4 - hp3)*0.5d0/Dv
+  ! First-order approximation for h_N, then compute derivatives at P = (u+Du/2, v+Dv/2)
+  h_N = h_W + h_E - h_S
+  h_P = 0.5d0*(h_S + h_N)
+  !h_P = 0.5*(h_E + h_W)
+  !h_P = 0.25*(h_S + h_N + h_E + h_W)
+  dhdu_P = (h_W - h_S + h_N - h_E)*0.5d0/Du
+  dhdv_P = (h_E - h_S + h_N - h_W)*0.5d0/Dv
 
-  ! Evaluate RHS at p5
-  call F(dhduvp5, hp5, dhdup5, dhdvp5)
+  ! Evaluate RHS at P
+  call F(dhduv_P, h_P, dhdu_P, dhdv_P)
 
-  ! Update hp4 using RHS
-  hp4 = hp3 + hp2 - hp1 + dhduvp5*Du*Dv
+  ! Update h_N using RHS
+  h_N = h_W + h_E - h_S + dhduv_P*Du*Dv
 
   ! Single Picard iteration for refinement (TODO: make this configurable)
   do j = 1, 1
-    hp4old = hp4
+    h_N_old = h_N
 
-     !  hp5 = 0.25*(hp1 + hp4 + hp2 + hp3)
-    dhdup5 = (hp3 - hp1 + hp4 - hp2)*0.5d0/Du
-    dhdvp5 = (hp2 - hp1 + hp4 - hp3)*0.5d0/Dv
+     !  h_P = 0.25*(h_S + h_N + h_E + h_W)
+    dhdu_P = (h_W - h_S + h_N - h_E)*0.5d0/Du
+    dhdv_P = (h_E - h_S + h_N - h_W)*0.5d0/Dv
 
-    ! Re-evaluate RHS at p5
-    call F(dhduvp5new, hp5, dhdup5, dhdvp5)
+    ! Re-evaluate RHS at P
+    call F(dhduv_P_new, h_P, dhdu_P, dhdv_P)
 
-    ! Final update to hp4
-    hp4 = hp3 + hp2 - hp1 + Du*Dv*dhduvp5new
+    ! Final update to h_N
+    h_N = h_W + h_E - h_S + Du*Dv*dhduv_P_new
   end do
 
 
