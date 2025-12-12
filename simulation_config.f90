816231b832ad237f6c8cca1cee730883baafc1fa
Miguel Zilhao <mzilhao@ua.pt>
2025-12-12 18:57:12 +0000

    simulation_config: fix indentation
    no functional changes

--- simulation_config.f90
+++ simulation_config.f90
@@ -38,43 +38,43 @@ module simulation_config_mod
 
 contains
 
-  !> Initialize simulation configuration with default values
-  !! Sets reasonable defaults for a mass inflation simulation.
-  !! Users can override individual fields after calling this.
-  subroutine init_simulation_config(sim_cfg)
-    type(simulation_config), intent(out) :: sim_cfg
+!> Initialize simulation configuration with default values
+!! Sets reasonable defaults for a mass inflation simulation.
+!! Users can override individual fields after calling this.
+subroutine init_simulation_config(sim_cfg)
+  type(simulation_config), intent(out) :: sim_cfg
 
-    ! Integration domain
-    sim_cfg%u0 = 0.0d0
-    sim_cfg%v0 = 5.0d0
-    sim_cfg%uf = 30.0d0
-    sim_cfg%vf = 10.0d0
+  ! Integration domain
+  sim_cfg%u0 = 0.0d0
+  sim_cfg%v0 = 5.0d0
+  sim_cfg%uf = 30.0d0
+  sim_cfg%vf = 10.0d0
 
-    ! Integration step sizes
-    sim_cfg%du = 0.01d0
-    sim_cfg%dv = 0.0005d0
+  ! Integration step sizes
+  sim_cfg%du = 0.01d0
+  sim_cfg%dv = 0.0005d0
 
-    ! AMR parameters
-    sim_cfg%AMR = .true.
-    sim_cfg%reldiff_max = 0.0001d0
+  ! AMR parameters
+  sim_cfg%AMR = .true.
+  sim_cfg%reldiff_max = 0.0001d0
 
-    ! Output resolution (print every Nth point)
-    sim_cfg%resu = 20
-    sim_cfg%resv = 20
+  ! Output resolution (print every Nth point)
+  sim_cfg%resu = 20
+  sim_cfg%resv = 20
 
-    ! Progress reporting cadence (stdout)
-    sim_cfg%progress_stride = 100
-    sim_cfg%progress_header_stride = 5000
+  ! Progress reporting cadence (stdout)
+  sim_cfg%progress_stride = 100
+  sim_cfg%progress_header_stride = 5000
 
-    ! Initial condition scale
-    sim_cfg%m0 = 1.0d0
+  ! Initial condition scale
+  sim_cfg%m0 = 1.0d0
 
-    ! Compute grid dimensions
-    sim_cfg%Nu = int((sim_cfg%uf - sim_cfg%u0) / sim_cfg%du + 1.001d0)
-    sim_cfg%Nv = int((sim_cfg%vf - sim_cfg%v0) / sim_cfg%dv + 1.001d0)
+  ! Compute grid dimensions
+  sim_cfg%Nu = int((sim_cfg%uf - sim_cfg%u0) / sim_cfg%du + 1.001d0)
+  sim_cfg%Nv = int((sim_cfg%vf - sim_cfg%v0) / sim_cfg%dv + 1.001d0)
 
-    ! Estimate maximum array allocation size (used for AMR refinement)
-    sim_cfg%Nu_max = int(2.0d0 * (sim_cfg%uf - sim_cfg%u0) / sim_cfg%reldiff_max)
-  end subroutine init_simulation_config
+  ! Estimate maximum array allocation size (used for AMR refinement)
+  sim_cfg%Nu_max = int(2.0d0 * (sim_cfg%uf - sim_cfg%u0) / sim_cfg%reldiff_max)
+end subroutine init_simulation_config
 
 end module simulation_config_mod
